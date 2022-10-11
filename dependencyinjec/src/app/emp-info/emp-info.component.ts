de89a3f711dff474a3e1241191031fe7c478dff4
devpop <impopcity@gmail.com>
2022-10-11 21:07:22 +0530

    0000

--- dependencyinjec/src/app/emp-info/emp-info.component.ts
+++ dependencyinjec/src/app/emp-info/emp-info.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { RecordsService } from '../records.service';
 
 @Component({
   selector: 'app-emp-info',
   templateUrl: './emp-info.component.html',
-  styleUrls: ['./emp-info.component.css']
+  styleUrls: ['./emp-info.component.css'],
+  providers:[RecordsService]
 })
 export class EmpInfoComponent implements OnInit {
+  infoReceived1: string[]=[];
+  infoReceived2: string[]=[];
+  infoReceived3: string[]=[];
 
-  constructor() { }
+  getInfoFromServiceClass1(){
+    this.infoReceived1 = this.rservices.info1
+  }
+  getInfoFromServiceClass2(){
+    this.infoReceived2 = this.rservices.info2
+  }
+  getInfoFromServiceClass3(){
+    this.infoReceived3 = this.rservices.info3
+  }
+  constructor(private rservices: RecordsService) { }
 
   ngOnInit(): void {
   }
--- dependencyinjec/src/app/records.service.ts
+++ dependencyinjec/src/app/records.service.ts
@@ -4,6 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RecordsService {
+  info1:string[] = ["pavi",'E1234','pop@89.com']
+  info2:string[] = ["viki",'E1224','viki@890.com']
+  info3:string[] = ["nava",'E1284','nava@8999.com']
+
+
+  getinfo1():string[]{
+    return this.info1
+  }
+  getinfo2():string[]{
+    return this.info2
+  }
+  getinfo3():string[]{
+    return this.info3
+  }
 
   constructor() { }
+
 }
