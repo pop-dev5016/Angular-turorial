f084477a4171d41a309436e64031cd422c5b2157
devpop <impopcity@gmail.com>
2022-10-11 17:07:04 +0530

    two way binding

--- binding/src/app/app.component.ts
+++ binding/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'binding';
   public name = "pavithran"
   public topic = "data binding"
+  public value = ""
   public image = "/assets/pop.png"
   onclick(){
     console.log("u clicked man");
